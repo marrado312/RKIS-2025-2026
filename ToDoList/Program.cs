20537a495d199f5441a48307801a2e1ea171655e
Илья Фучаджи <twixtyz@gmial.com>
2025-10-07 18:42:26 +0300

    Lesson: 2

---
+++ ToDoList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнил Фучаджи и Клюев");
+
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите вашу Фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите вашу дату рождения: ");
+            string birthday = Console.ReadLine();
+
+            int birthday = int.Parse(Birthday);
+
+            int NowYear = DateTime.Now.Year;
+
+            int Age = NowYear - Birthday;
+
+            Console.WriteLine($"Добавлен пользователь {Name} {LastName}, Возвраст - {Age}");
+        }
+    }
+
+}
