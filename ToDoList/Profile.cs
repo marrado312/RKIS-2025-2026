6d1bc5eb3cdad3cc0bbb515e7f13d88e214657ab
Илья Фучаджи <twixtyz@gmial.com>
2025-10-20 21:51:20 +0300

    Lesson 6: создал класс profile с полями FirstName LastName BirthYear и методом GetInfo

---
+++ ToDoList/Profile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TodoList
+{
+    class Profile
+    {
+        private string firstName;
+        private string lastName;
+        private int birthYear;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+        public int BirthYear
+        {
+            get { return birthYear; }
+            private set { birthYear = value; }
+        }
+
+        public Profile(string firstName, string lastName, int birthYear)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthYear = birthYear;
+        }
+
+        public string GetInfo()
+        {
+            int age = DateTime.Now.Year - birthYear;
+            return $"{firstName} {lastName}, возраст {age}";
+        }
+    }
+}
+
