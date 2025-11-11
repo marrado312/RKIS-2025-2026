626e92b9ebe5cb7227e82e36d34b8e453f8bd8d1
Илья Фучаджи <twixtyz@gmial.com>
2025-11-11 19:02:08 +0300

    Lesson 9: создал команду status, изменил справку help, добавил метод setstatus

--- ToDoList/Commands/HelpCommand.cs
+++ ToDoList/Commands/HelpCommand.cs
@@ -13,6 +13,7 @@ namespace TodoList
         view - список задач (--index/-i, --status/-s, --update-date/-d, --all/-a)
         read - полный текст задачи
         done - отметить выполненной
+		status - изменить статус задачи (notstarted, inprogress, completed, postponed, failed)
         delete - удалить задачу
         update - изменить текст задачи
         exit - выход из программы");
