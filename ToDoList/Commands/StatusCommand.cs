558e7d9a291855f6198a3c0a853e60c12e3d263e
Илья Фучаджи <twixtyz@gmial.com>
2025-11-19 19:26:11 +0300

    Lesson 10: добавил метод unexecute в statuscommand для отмены измениния статуса

--- ToDoList/Commands/StatusCommand.cs
+++ ToDoList/Commands/StatusCommand.cs
@@ -8,6 +8,9 @@ namespace TodoList.Commands
 		public int TaskIndex { get; set; }
 		public TodoStatus NewStatus { get; set; }
 
+		public TodoStatus oldStatus;
+		public int statusIndex;
+
 		public void Execute()
 		{
 			if (TaskIndex < 0 || TaskIndex >= TodoList.Count)
@@ -16,9 +19,21 @@ namespace TodoList.Commands
 				return;
 			}
 
+			statusIndex = TaskIndex;
+			oldStatus = TodoList[TaskIndex].Status;
+
 			var task = TodoList[TaskIndex];
 			task.SetStatus(NewStatus);
 			Console.WriteLine($"Статус задачи {TaskIndex + 1} изменен на: {task.GetStatusText()}");
+		}
+
+		public void Unexecute()
+		{
+			if (TodoList.Count > statusIndex)
+			{
+				TodoList[statusIndex].SetStatus(oldStatus);
+				Console.WriteLine($"Отменено изменение статуса задачи");
+			}
 		}
 	}
 }
