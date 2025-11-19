3ea98de37e7b56da6f59cc9944413e6ad2dc9fd3
Илья Фучаджи <twixtyz@gmial.com>
2025-11-19 19:30:37 +0300

    Lesson 10: добавил пустой метод unexecute в readcommand

--- ToDoList/Commands/ReadCommand.cs
+++ ToDoList/Commands/ReadCommand.cs
@@ -2,21 +2,26 @@
 
 namespace TodoList
 {
-    class ReadCommand : ICommand
-    {
-        public TodoList TodoList { get; set; }
-        public int TaskIndex { get; set; }
+	class ReadCommand : ICommand
+	{
+		public TodoList TodoList { get; set; }
+		public int TaskIndex { get; set; }
 
-        public void Execute()
-        {
-            try
-            {
-                TodoList.Read(TaskIndex);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ошибка: {ex.Message}");
-            }
-        }
-    }
+		public void Execute()
+		{
+			try
+			{
+				TodoList.Read(TaskIndex);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Ошибка: {ex.Message}");
+			}
+		}
+
+		public void Unexecute()
+		{
+			// пустой метод
+		}
+	}
 }
