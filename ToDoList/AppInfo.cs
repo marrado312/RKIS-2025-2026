eff346b86eea921ff204d4936e4912ab33676d16
Илья Фучаджи <twixtyz@gmial.com>
2025-11-19 19:05:55 +0300

    Lesson 10: создал статический класс AppInfo с полями todos, currentprofile и стеками undo и redo

---
+++ ToDoList/AppInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ToDoList;
+
+namespace TodoList
+{
+	public static class AppInfo
+	{
+		public static TodoList Todos { get; set; }
+		public static Profile CurrentProfile { get; set; }
+
+		public static Stack<ICommand> undoStack = new Stack<ICommand>();
+		public static Stack<ICommand> redoStack = new Stack<ICommand>();
+
+		static AppInfo()
+		{
+			Todos = new TodoList();
+			CurrentProfile = new Profile("", "", DateTime.Now.Year);
+			undoStack = new Stack<ICommand>();
+			redoStack = new Stack<ICommand>();
+		}
+	}
+}
