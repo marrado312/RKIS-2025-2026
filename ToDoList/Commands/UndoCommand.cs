56044a4a5d1d26f814ecbd854c99c8c4464e34fe
Илья Фучаджи <twixtyz@gmial.com>
2025-11-19 22:16:15 +0300

    Lesson 10: cделал команду undo для отмены действий

---
+++ ToDoList/Commands/UndoCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TodoList
+{
+	class UndoCommand : ICommand
+	{
+		public void Execute()
+		{
+			if (AppInfo.undoStack.Count > 0)
+			{
+				ICommand command = AppInfo.undoStack.Pop();
+				command.Unexecute();
+				AppInfo.redoStack.Push(command);
+				Console.WriteLine("Отменено последнее действие");
+			}
+			else
+			{
+				Console.WriteLine("Нет действий для отмены");
+			}
+		}
+
+		public void Unexecute()
+		{
+			// пустой метод
+		}
+	}
+}
