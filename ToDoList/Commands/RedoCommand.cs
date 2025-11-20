4b44d08369d701f78d5c338dc2ae3bac908b701f
Илья Фучаджи <twixtyz@gmial.com>
2025-11-20 22:21:09 +0300

    Lesson 10: добавил команду redo для повтора отмененных действий

---
+++ ToDoList/Commands/RedoCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TodoList
+{
+	class RedoCommand : ICommand
+	{
+		public void Execute()
+		{
+			if (AppInfo.redoStack.Count > 0)
+			{
+				ICommand command = AppInfo.redoStack.Pop();
+				command.Execute();
+				AppInfo.undoStack.Push(command);
+				Console.WriteLine("Повторено последнее отмененное действие");
+			}
+			else
+			{
+				Console.WriteLine("Нет действий для повтора");
+			}
+		}
+
+		public void Unexecute()
+		{
+			// пустой метод
+		}
+	}
+}
