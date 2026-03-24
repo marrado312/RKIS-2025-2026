c967552a3e1b302e205dec7b608a0e33f154975d
Илья Фучаджи <twixtyz@gmial.com>
2026-03-24 21:09:53 +0300

    задание 1: команда load в статическом констукторе commandparser

---
+++ ToDoList/Commands/LoadCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoList.Exceptions;
+
+namespace TodoList.Commands
+{
+	public class LoadCommand : ICommand
+	{
+		private string _args;
+
+		public LoadCommand(string args)
+		{
+			_args = args;
+		}
+
+		public void Execute()
+		{
+		}
+	}
+}
