0c07fba04775d07fca4e97c819052a9b4674618e
Илья Фучаджи <twixtyz@gmial.com>
2026-03-31 21:41:09 +0300

    Лекция 4, добавил исключение StorageExcpetion

---
+++ ToDoList/Exceptions/StorageException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TodoList
+{
+	public class StorageException : Exception
+	{
+		public StorageException(string message) : base(message) { }
+		public StorageException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
