22bb601b7348259024be3d0693c7b547a9389064
Илья Фучаджи <twixtyz@gmial.com>
2026-03-31 21:25:58 +0300

    Лекция 4, создал интерфейс IDataStorage для отвязки логики

---
+++ ToDoList/Interfaces/IDataStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Interfaces;
+internal interface IDataStorage
+{
+}
