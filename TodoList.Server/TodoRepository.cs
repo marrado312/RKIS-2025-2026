a89f0eb44431def004adf8eeffa405de5b7554b4
Ilyastud <twixtyz@gmial.com>
2026-04-29 21:52:45 +0300

    лекция 6: Создал todorepository и сделал метод инициализации таблиц

---
+++ TodoList.Server/TodoRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace TodoList.Server
+{
+	public class TodoRepository
+	{
+		private readonly Database _db;
+
+		public TodoRepository(Database db) => _db = db;
+
+		// создание таблиц
+		public async Task InitializeAsync()
+		{
+			using var connection = _db.CreateConnection();
+			await connection.OpenAsync();
+
+			var command = connection.CreateCommnand();
+			command.CommandText = @"
+                CREATE TABLE IF NOT EXISTS Users (Id TEXT PRIMARY KEY, Data BLOB);
+                CREATE TABLE IF NOT EXISTS Todos (UserId TEXT PRIMARY KEY, Data BLOB);";
+			await command.ExecuteNonQueryAsync();
+
+			Console.WriteLine("=== БАЗА ДАННЫХ ИНИЦИАЛИЗИРОВАНА ===");
+		}
+	}
+}
+
+
