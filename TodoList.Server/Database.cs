29bdc18b117b4688a44c714cdfef4ea117725075
Ilyastud <twixtyz@gmial.com>
2026-04-29 21:48:35 +0300

    лекция 6: добавил класс database

---
+++ TodoList.Server/Database.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace TodoList.Server
+{
+
+	public class Database
+	{
+		public string ConnectionString { get; }
+
+		public Database(string connectionString)
+		{
+			ConnectionString = connectionString;
+		}
+
+		public SqliteConnection CreateConnection() => new SqliteConnection(ConnectionString);
+	}
+}
